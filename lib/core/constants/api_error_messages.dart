68be53650b9e24743269521b75d1b4b2f7827532
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-04-04 08:38:54 +0200

    feat(networking): Add API routes, error messages, and HTTP status codes

---
+++ lib/core/constants/api_error_messages.dart
@@ -0,0 +1,10 @@
+class ApiErrorMessages {
+  static const String badRequest = 'BadRequest';
+  static const String noContent = 'NoContent';
+  static const String forbidden = 'Forbidden';
+  static const String unauthorized = 'Unauthorized';
+  static const String notFound = 'NotFound';
+  static const String conflict = 'Conflict';
+  static const String internalServerError = 'InternalServerError';
+  static const String unknownError = 'UnknownError';
+}
