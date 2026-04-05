ba3a2b725b60f1848ef90e43da77c307b2a5777b
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-04-05 09:28:13 +0200

    feat(wallet): Add action button to app bar flexible space

--- lib/modules/wallet/presentation/pages/wallet_page.dart
+++ lib/modules/wallet/presentation/pages/wallet_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import '../../../../core/constants/app_config.dart';
 import '../../../../core/di/di.dart';
 import '../../../../core/services/l10n/l10n_service.dart';
+import '../../../../core/widgets/buttons/outlined_button/outlined_button_component.dart';
 import '../../../../core/widgets/slivers/sliver_app_bar/sliver_app_bar_with_waves_component.dart';
 import '../widget/payment_methods.dart';
 
@@ -24,6 +26,17 @@ class WalletPage extends HookWidget {
             scrollController: scrollController,
             expandedHeight: 320.h,
             title: l10n.wallet,
+            flexibleSpace: Padding(
+              padding: const EdgeInsets.only(
+                top: kToolbarHeight + AppConfig.padding * 2,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  OutlinedButtonComponent(label: '', onPressed: () {}),
+                ],
+              ),
+            ),
           ),
           const PaymentMethods(),
         ],
