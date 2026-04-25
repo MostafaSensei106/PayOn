4f3096887009fb375ae72d50afd1b9eeb0f845b7
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-04-25 11:47:34 +0300

    feat: Add SVG assets and integrate `flutter_svg` package
    - Add 12 new SVG illustrations to `assets/svgs/` (e.g., language, fingerprint, send money).
    - Define constant paths for all new SVGs in `SvgsImages` class within `lib/core/constants/svgs_images.dart`.
    - Add `flutter_svg: ^2.2.4` dependency to `pubspec.yaml`.

--- lib/core/constants/svgs_images.dart
+++ lib/core/constants/svgs_images.dart
@@ -1,1 +1,37 @@
-final class SvgsImages {}
+final class SvgsImages {
+  static const String languageSvg =
+      'assets/svgs/undraw_around-the-world_1p8h.svg';
+
+  static const String completeSvg =
+      'assets/svgs/undraw_complete-design_pzh6.svg';
+
+  static const String designerMindsetSvg =
+      'assets/svgs/undraw_designer-mindset_bxms.svg';
+
+  static const String developerAvatarSvg =
+      'assets/svgs/undraw_developer-avatar_f6ac.svg';
+
+  static const String fingerprintSvg =
+      'assets/svgs/undraw_fingerprint_kdwq.svg';
+
+  static const String forgotPasswordSvg =
+      'assets/svgs/undraw_forgot-password_nttj.svg';
+
+  static const String notificationsSvg =
+      'assets/svgs/undraw_my-notifications_fy5v.svg';
+
+  static const String questionsSvg =
+      'assets/svgs/undraw_questions_52ic.svg';
+
+  static const String sendMoneySvg =
+      'assets/svgs/undraw_send-money_4qc7.svg';
+
+  static const String twoFactorAuthSvg =
+      'assets/svgs/undraw_two-factor-authentication_ofho.svg';
+
+  static const String unboxingSvg =
+      'assets/svgs/undraw_unboxing_p8sg.svg';
+
+  static const String updateSvg =
+      'assets/svgs/undraw_update_jvp1.svg';
+}
