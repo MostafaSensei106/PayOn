efc82b2f83343848db7197e5802a38644de9fa4a
MostafaSensei106 <wwwmostafagmer100'gmail.com>
2026-06-09 03:33:38 +0300

    feat: Add CreateTransactionDraftRequestBody and CreateTransactionDraftResponseBody models with JSON serialization support

---
+++ lib/modules/send_money/data/models/create_transaction_draft/create_transaction_draft_response_body.dart
@@ -0,0 +1,6 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'create_transaction_draft_response_body.g.dart';
+
+@JsonSerializable()
+class CreateTransactionDraftResponseBody {}
---
+++ lib/modules/send_money/data/models/create_transaction_draft/create_transaction_draft_response_body.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'create_transaction_draft_response_body.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CreateTransactionDraftResponseBody _$CreateTransactionDraftResponseBodyFromJson(
+  Map<String, dynamic> json,
+) => CreateTransactionDraftResponseBody();
+
+Map<String, dynamic> _$CreateTransactionDraftResponseBodyToJson(
+  CreateTransactionDraftResponseBody instance,
+) => <String, dynamic>{};
